4b0092dc02107cbc5aff6f4ed60a6db373f6b1e1
jalemy <jalemy.xps@gmail.com>
2019-06-12 22:30:47 +0900

    gulp周り一式設定

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssdeclsort = require('css-declaration-sorter');
+var mqpacker = require('css-mqpacker');
+var cssImport = require('postcss-import');
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/**/*.scss')
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(postcss([cssImport()]))
+    .pipe(postcss([cssdeclsort({ order: 'smacss' })]))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
